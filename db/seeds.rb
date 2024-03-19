b8c0784f6283553791f141d61b4c5b099f3f5af6
David Cahez <cahez.david@gmail.com>
2024-03-19 19:20:50 +1000

    done seeding Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating Fake restaurants'
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: %w[chinese italian japanese french belgian][rand(5)]
+  )
+  puts "Created #{restaurant.name}, a wonderful #{restaurant.category} restaurant, located at #{restaurant.address}."
+end
+
+puts 'Done with Faking restaurants!'
